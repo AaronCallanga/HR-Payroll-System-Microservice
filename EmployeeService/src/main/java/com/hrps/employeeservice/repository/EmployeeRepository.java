ec46938c54ed4542253db44731f3a1dff97b19d7
pogi <aaroncallanga01@gmail.com>
2025-10-05 00:38:53 +0800

    Employee Service - Employee  repository

---
+++ EmployeeService/src/main/java/com/hrps/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.hrps.employeeservice.repository;
+
+import com.hrps.employeeservice.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+}
