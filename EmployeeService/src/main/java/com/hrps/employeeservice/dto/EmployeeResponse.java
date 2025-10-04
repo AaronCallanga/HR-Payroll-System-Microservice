01ec9c6eca243ad2f4e684ae1fb595e0f137538c
pogi <aaroncallanga01@gmail.com>
2025-10-05 00:48:11 +0800

    Employee Service - EmployeeResponse

--- EmployeeService/src/main/java/com/hrps/employeeservice/dto/EmployeeResponse.java
+++ EmployeeService/src/main/java/com/hrps/employeeservice/dto/EmployeeResponse.java
@@ -17,7 +17,112 @@ public class EmployeeResponse {
     private LocalDate hireDate;
     private String role;
     private Double salary;
-    private String status = "ACTIVE";
+    private String status;
 
     private DepartmentResponse department;
+
+    public EmployeeResponse() {
+    }
+
+    public EmployeeResponse(UUID id,
+                            String firstName,
+                            String lastName,
+                            String email,
+                            String phoneNumber,
+                            LocalDate hireDate,
+                            String role,
+                            Double salary,
+                            String status,
+                            DepartmentResponse department) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hireDate = hireDate;
+        this.role = role;
+        this.salary = salary;
+        this.status = status;
+        this.department = department;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public DepartmentResponse getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(DepartmentResponse department) {
+        this.department = department;
+    }
 }
