37a7879caf1c6f46a9c527a6851a0256c51fdb15
pogi <aaroncallanga01@gmail.com>
2025-10-05 00:37:39 +0800

    Employee Service - added getter setter constructor to models

--- EmployeeService/src/main/java/com/hrps/employeeservice/model/Department.java
+++ EmployeeService/src/main/java/com/hrps/employeeservice/model/Department.java
@@ -24,4 +24,69 @@ public class Department {
 
     @OneToMany(mappedBy = "department")
     private List<Employee> employees;
+
+    public Department() {
+    }
+
+    public Department(Long id,
+                      String name,
+                      String description,
+                      LocalDateTime updatedAt,
+                      LocalDateTime createdAt,
+                      List<Employee> employees) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
--- EmployeeService/src/main/java/com/hrps/employeeservice/model/Employee.java
+++ EmployeeService/src/main/java/com/hrps/employeeservice/model/Employee.java
@@ -35,4 +35,129 @@ public class Employee {
 
     private LocalDateTime createdAt = LocalDateTime.now();
     private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public Employee() {
+    }
+
+    public Employee(UUID id,
+                    String firstName,
+                    String email,
+                    String lastName,
+                    LocalDateTime updatedAt,
+                    LocalDateTime createdAt,
+                    Department department,
+                    String status,
+                    Double salary,
+                    String role,
+                    LocalDate hireDate,
+                    String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.email = email;
+        this.lastName = lastName;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
+        this.department = department;
+        this.status = status;
+        this.salary = salary;
+        this.role = role;
+        this.hireDate = hireDate;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
