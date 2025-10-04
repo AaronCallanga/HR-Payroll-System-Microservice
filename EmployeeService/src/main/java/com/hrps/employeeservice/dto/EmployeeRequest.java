9c2c7d87c4f145a6ede5c154223500760c9cc2ac
pogi <aaroncallanga01@gmail.com>
2025-10-05 00:51:45 +0800

    Employee Service - EmployeeRequest

---
+++ EmployeeService/src/main/java/com/hrps/employeeservice/dto/EmployeeRequest.java
@@ -0,0 +1,110 @@
+package com.hrps.employeeservice.dto;
+
+import java.time.LocalDate;
+
+public class EmployeeRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate hireDate;
+    private String role;
+    private Double salary;
+    private String status;
+    private Long departmentId;
+
+    public EmployeeRequest() {
+    }
+
+    public EmployeeRequest(String firstName,
+                           String lastName,
+                           String email,
+                           String phoneNumber,
+                           LocalDate hireDate,
+                           String role,
+                           Double salary,
+                           String status,
+                           Long departmentId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hireDate = hireDate;
+        this.role = role;
+        this.salary = salary;
+        this.status = status;
+        this.departmentId = departmentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+}
--- EmployeeService/src/main/java/com/hrps/employeeservice/service/EmployeeService.java
+++ EmployeeService/src/main/java/com/hrps/employeeservice/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.hrps.employeeservice.service;
 
+import com.hrps.employeeservice.dto.EmployeeRequest;
 import com.hrps.employeeservice.dto.EmployeeResponse;
 import com.hrps.employeeservice.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
