eaa5985746b8762b3ad6ec909b63bd19662eab7d
pogi <aaroncallanga01@gmail.com>
2025-10-05 20:18:00 +0800

    Employee Service EmplyoeeEventProducer

---
+++ EmployeeService/src/main/java/com/hrps/employeeservice/kafka/EmployeeEventProducer.java
@@ -0,0 +1,32 @@
+package com.hrps.employeeservice.kafka;
+
+import com.hrps.avro.employee.EmployeeEvent;
+import com.hrps.employeeservice.model.Employee;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeEventProducer {
+    private final KafkaTemplate<String, EmployeeEvent> kafkaTemplate;
+    private static final String TOPIC = "employee.events";
+
+    public EmployeeEventProducer(KafkaTemplate<String, EmployeeEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEmployeeCreatedEvent(Employee employee) {
+        EmployeeEvent event = EmployeeEvent.newBuilder()
+                                           .setId(UUID.randomUUID().toString())
+                                           .setFirstName("John")
+                                           .setLastName("Doe")
+                                           .setEmail("john@company.com")
+                                           .setDepartmentName("Engineering")
+                                           .setEventType("CREATED")
+                                           .build();
+
+        kafkaTemplate.send(TOPIC, event.getId().toString(), event);
+        System.out.println("✅ Sent EmployeeEvent to Kafka: " + event);
+    }
+}
