66ca30da6e3e1ce7352a64139e112d3a71417132
pogi <aaroncallanga01@gmail.com>
2025-10-06 12:47:01 +0800

    EmployeeService - Payroll model

--- EmployeeService/src/main/java/com/hrps/employeeservice/controller/EmployeeController.java
+++ EmployeeService/src/main/java/com/hrps/employeeservice/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
