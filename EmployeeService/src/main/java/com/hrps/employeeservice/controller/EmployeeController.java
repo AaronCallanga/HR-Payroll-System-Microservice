227a6e4bd6438a586c5f765962a50a4f28ac7798
pogi <aaroncallanga01@gmail.com>
2025-10-06 12:17:49 +0800

    EmployeeService - EmployeeController getAllEmployees

--- EmployeeService/src/main/java/com/hrps/employeeservice/controller/EmployeeController.java
+++ EmployeeService/src/main/java/com/hrps/employeeservice/controller/EmployeeController.java
@@ -32,6 +32,6 @@ public class EmployeeController {
 
     @GetMapping
     public ResponseEntity<List<EmployeeResponse>> getEmployees() {
-        return ResponseEntity.ok(employeeService.)
+        return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 }
