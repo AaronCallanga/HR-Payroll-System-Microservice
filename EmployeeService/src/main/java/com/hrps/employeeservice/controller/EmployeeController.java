c0351a9afd7a043815bbee5189d435e5e571e1d5
pogi <aaroncallanga01@gmail.com>
2025-10-06 11:49:53 +0800

    EmployeeService - EmployeeController

---
+++ EmployeeService/src/main/java/com/hrps/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.hrps.employeeservice.controller;
+
+import com.hrps.employeeservice.dto.EmployeeRequest;
+import com.hrps.employeeservice.dto.EmployeeResponse;
+import com.hrps.employeeservice.model.Employee;
+import com.hrps.employeeservice.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponse> addEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return ResponseEntity.ok(employeeService.addEmployee(employeeRequest));
+    }
+}
