1855b097786f8fe5929249470b61921bf6213944
pogi <aaroncallanga01@gmail.com>
2025-10-10 00:03:20 +0800

    attendnace-service leave quota request DTO

---
+++ attendance-service/src/main/java/com/hrps/attendanceservice/dto/LeaveQuotaRequestDTO.java
@@ -0,0 +1,45 @@
+package com.hrps.attendanceservice.dto;
+
+import java.util.UUID;
+
+/**
+ * DTO for creating or updating leave quota for an employee.
+ */
+public class LeaveQuotaRequestDTO {
+    private UUID employeeId;
+    private int totalLeaves;
+    private int usedLeaves;
+
+    public LeaveQuotaRequestDTO() {
+    }
+
+    public LeaveQuotaRequestDTO(UUID employeeId, int totalLeaves, int usedLeaves) {
+        this.employeeId = employeeId;
+        this.totalLeaves = totalLeaves;
+        this.usedLeaves = usedLeaves;
+    }
+
+    public UUID getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(UUID employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getTotalLeaves() {
+        return totalLeaves;
+    }
+
+    public void setTotalLeaves(int totalLeaves) {
+        this.totalLeaves = totalLeaves;
+    }
+
+    public int getUsedLeaves() {
+        return usedLeaves;
+    }
+
+    public void setUsedLeaves(int usedLeaves) {
+        this.usedLeaves = usedLeaves;
+    }
+}
