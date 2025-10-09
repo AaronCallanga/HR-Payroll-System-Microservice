fc3a491e081700c28daeeb25b71df4549a5611ea
pogi <aaroncallanga01@gmail.com>
2025-10-10 00:29:51 +0800

    Attendance service leave history repository

--- attendance-service/src/main/java/com/hrps/attendanceservice/repository/LeaveHistoryRepository.java
+++ attendance-service/src/main/java/com/hrps/attendanceservice/repository/LeaveHistoryRepository.java
@@ -1,11 +1,11 @@
 package com.hrps.attendanceservice.repository;
 
-import com.hrps.attendanceservice.entity.AttendanceRecord;
+import com.hrps.attendanceservice.entity.LeaveHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AttendanceRecordRepository extends JpaRepository<AttendanceRecord, UUID> {
+public interface LeaveHistoryRepository extends JpaRepository<LeaveHistory, UUID> {
 }
