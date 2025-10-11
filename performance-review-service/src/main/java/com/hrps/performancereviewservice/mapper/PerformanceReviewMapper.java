9c91c994e85252223ccbb984281ba45e239c62db
pogi <aaroncallanga01@gmail.com>
2025-10-11 23:51:18 +0800

    Performance Review Service - Performance Review mapper

---
+++ performance-review-service/src/main/java/com/hrps/performancereviewservice/mapper/PerformanceReviewMapper.java
@@ -0,0 +1,33 @@
+package com.hrps.performancereviewservice.mapper;
+
+import com.hrps.performancereviewservice.dto.PerformanceReviewRequestDTO;
+import com.hrps.performancereviewservice.dto.PerformanceReviewResponseDTO;
+import com.hrps.performancereviewservice.entity.PerformanceReview;
+
+public class PerformanceReviewMapper {
+
+    // Convert request to entity
+    public static PerformanceReview toEntity(PerformanceReviewRequestDTO request) {
+        PerformanceReview review = new PerformanceReview();
+        review.setEmployeeId(request.getEmployeeId());
+        review.setReviewPeriod(request.getReviewPeriod());
+        review.setReviewer(request.getReviewer());
+        review.setScore(request.getScore());
+        review.setComments(request.getComments());
+        review.setStatus("PENDING");
+        return review;
+    }
+
+    // Convert entity to response
+    public static PerformanceReviewResponseDTO toResponse(PerformanceReview review) {
+        PerformanceReviewResponseDTO res = new PerformanceReviewResponseDTO();
+        res.setId(review.getId());
+        res.setEmployeeId(review.getEmployeeId());
+        res.setReviewPeriod(review.getReviewPeriod());
+        res.setReviewer(review.getReviewer());
+        res.setScore(review.getScore());
+        res.setComments(review.getComments());
+        res.setStatus(review.getStatus());
+        return res;
+    }
+}
