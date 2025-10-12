85e75816748c7b0c21711be7371d6809bd8a160c
pogi <aaroncallanga01@gmail.com>
2025-10-13 01:41:51 +0800

    Auth Service - LoginResponseDTO

---
+++ auth-service/src/main/java/com/hrps/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.hrps.authservice.dto;
+
+public class LoginResponseDTO {
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
